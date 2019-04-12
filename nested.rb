b1c350bf80cb3ccc9dda79f34386001934c33201
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 00:43:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -35,7 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-     
+  #solution below   
   programmer_hash[:alan_kay][:known_for]
 end
 
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
+  #solution below   
   programmer_hash[:dennis_ritchie][:language].first
 end
 
@@ -75,8 +75,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  #solution below
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  programmer_hash
 end
 
 def changing_alan
@@ -96,9 +97,8 @@ def changing_alan
         }
      }
    
-     alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for] = alans_new_info
-     
+      alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
