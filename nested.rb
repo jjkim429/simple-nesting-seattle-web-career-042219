330f36aff0a6f76e65ae073a4059a3a3796b14ae
Jennifer Kim <jjkim429@gmail.com>
2019-04-12 00:39:51 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -117,7 +117,7 @@ def adding_to_dennis
         }
      }
   
-  programmer_hash[:dennis_ritchie][:languages] => ["C", "Assembley"]
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembley"]
   return programmer_hash[:dennis_ritchie][:languages]
 
 end
